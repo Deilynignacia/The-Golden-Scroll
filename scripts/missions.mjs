fc68648cc57326fd6c95d0d6d0a4e471df031806
Deilyn Zamudio <deilynzamudio@gmail.com>
2026-02-17 02:20:52 -0300

    locked & unlocked

--- scripts/missions.mjs
+++ scripts/missions.mjs
@@ -1,10 +1,24 @@
-// Select Buttons
+// Get data from LocalStorage
+const teamData = JSON.parse(localStorage.getItem("goldenScroll_team"));
 const missionButtons = document.querySelectorAll('.mission-btn');
 
+//Buttons Logic
 missionButtons.forEach(button => {
+    const id = button.getAttribute('data-id');
+    const status = teamData?.progress?.[id] || "locked";
+
+    if (status === "locked") {
+        // Locked
+        button.classList.add('is-locked');
+        button.setAttribute('disabled', 'true');
+    }
+
+    // Button can only be clicked if it is unlocked
     button.addEventListener('click', () => {
-        // From HTML
-        const id = button.getAttribute('data-id');
-        window.location.href = `mission-details.html?id=${id}`;
+        if (status === "unlocked") {
+            window.location.href = `mission-details.html?id=${id}`;
+        } else {
+            console.log(`Mission ${id} is locked at the moment.`);
+        }
     });
 });
